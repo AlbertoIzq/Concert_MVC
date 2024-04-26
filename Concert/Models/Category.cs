021f1e85fbc5e5cd31b754fb26a1657fc90a0c0e
Alberto <alberto.izquierdomo@gmail.com>
2024-04-26 14:31:17 +0200

    Start adding database initial configuration

---
+++ Concert/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
--- Concert/Program.cs
+++ Concert/Program.cs
@@ -1,7 +1,14 @@
+using ConcertWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Add the database service
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
