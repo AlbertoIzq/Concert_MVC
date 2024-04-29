6df9aa68bdd92ed8ddd87e2ad13c5aafec25e5c5
Alberto <alberto.izquierdomo@gmail.com>
2024-04-29 14:52:11 +0200

    Add a table of all categories in view

--- Concert/Controllers/CategoryController.cs
+++ Concert/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConcertWeb.Data;
+using ConcertWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Concert.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
         }
     }
 }
