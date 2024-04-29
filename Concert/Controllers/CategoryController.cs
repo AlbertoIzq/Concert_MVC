956271bfc888f0c87255f1d6d530c8f7a14a6b9f
Alberto <alberto.izquierdomo@gmail.com>
2024-04-29 14:06:10 +0200

    Add Category link in header

---
+++ Concert/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Concert.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
