e48c91629371ccd9cc04785d384cfb388bcc282a
Alberto <alberto.izquierdomo@gmail.com>
2024-04-29 14:06:23 +0200

    Seed Category table

--- Concert/Data/ApplicationDbContext.cs
+++ Concert/Data/ApplicationDbContext.cs
@@ -13,5 +13,15 @@ namespace ConcertWeb.Data
 
         // To create a table for Category data model
         public DbSet<Category> Categories { get; set; }
+
+        // To seed Category table with some data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id = 1, Name = "Chanson française", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "Disco polo", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "Reggae", DisplayOrder = 3 }
+            );
+        }
     }
 }
--- Concert/Migrations/20240429120121_SeedCategoryTable.Designer.cs
+++ Concert/Migrations/20240429120121_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using ConcertWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Concert.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240429120121_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
