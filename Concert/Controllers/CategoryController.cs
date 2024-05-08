5f8ec5a747ee63338845f878c2c3ab019bcc2b71
Alberto <alberto.izquierdomo@gmail.com>
2024-05-08 19:21:11 +0200

    Add toastr notifications when Category is created, updated or deleted

--- Concert/Controllers/CategoryController.cs
+++ Concert/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace Concert.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -66,6 +67,7 @@ namespace Concert.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -100,6 +102,7 @@ namespace Concert.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
