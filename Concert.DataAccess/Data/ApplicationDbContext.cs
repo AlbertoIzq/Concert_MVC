85f3d0ef7cc69243edc50cbf94aa45ad8e3fa73d
Alberto <alberto.izquierdomo@gmail.com>
2024-06-02 13:32:16 +0200

    Create and seed Product table in db

--- Concert.DataAccess/Data/ApplicationDbContext.cs
+++ Concert.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,9 @@ namespace Concert.DataAccess.Data
         // To create a table for Category data model
         public DbSet<Category> Categories { get; set; }
 
+        // To create a table for Product data model
+        public DbSet<Product> Products { get; set; }
+
         // To seed Category table with some data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,6 +24,33 @@ namespace Concert.DataAccess.Data
                 new Category() { Id = 1, Name = "Chanson française", DisplayOrder = 1 },
                 new Category() { Id = 2, Name = "Disco polo", DisplayOrder = 2 },
                 new Category() { Id = 3, Name = "Reggae", DisplayOrder = 3 }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    Id = 1,
+                    Artist = "Ace of base",
+                    Title = "All that she wants",
+                    Length = new TimeSpan(0, 2, 6),
+                    ReleaseYear = 1992
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Artist = "And one",
+                    Title = "Military fashion show",
+                    Length = new TimeSpan(0, 2, 19),
+                    ReleaseYear = 2006
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Artist = "Boys",
+                    Title = "Szalona",
+                    Length = new TimeSpan(0, 2, 52),
+                    ReleaseYear = 1997
+                }
             );
         }
     }
