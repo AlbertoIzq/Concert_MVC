16d283b79e46d629a644ed041092ef788a6f32e0
Alberto <alberto.izquierdomo@gmail.com>
2024-06-02 13:30:27 +0200

    Create Product Model

--- Concert.Models/Category.cs
+++ Concert.Models/Category.cs
@@ -7,10 +7,12 @@ namespace Concert.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Required field.")]
         [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
+
         [Required]
         [Range(1, 100, ErrorMessage = "It must be between 1 and 100.")]
         [DisplayName("Display Order")]
---
+++ Concert.Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Concert.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required field.")]
+        [MaxLength(100)]
+        [DisplayName("Artist Name")]
+        public string Artist { get; set; }
+
+        [Required(ErrorMessage = "Required field.")]
+        [MaxLength(100)]
+        [DisplayName("Song Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Required field.")]
+        [DisplayName("Song Length")]
+        public TimeSpan Length { get; set; }
+
+        [Required(ErrorMessage = "Required field.")]
+        [Range(1900, 2100, ErrorMessage = "It must be between 1900 and 2100.")]
+        [DisplayName("Release Year")]
+        public int ReleaseYear { get; set; }
+
+        /*
+        // TODO because these properties are linked with other models
+        [MaxLength(30)]
+        [DisplayName("Music Genre")]
+        public string Genre { get; set; }
+        
+        [MaxLength(30)]
+        public string Language { get; set; }
+        */
+    }
+}
