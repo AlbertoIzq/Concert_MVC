6f5d7f3b3164e87bfcccb224c2732e78bdc829ba
Alberto <alberto.izquierdomo@gmail.com>
2024-06-04 17:10:37 +0200

    Replace Song Index table by a TableData. Edit and Delete butons to be added again afterwards

---
+++ Concert/wwwroot/js/song.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tableData').DataTable({
+        "ajax": { url: '/admin/song/getall' },
+        "columns": [
+            { data: 'artist', "width": "20%" },
+            { data: 'title', "width": "20%" },
+            { data: 'length', "width": "15%" },
+            { data: 'releaseYear', "width": "15%" },
+            { data: 'genre.name', "width": "15%" },
+        ]
+    });
+}
