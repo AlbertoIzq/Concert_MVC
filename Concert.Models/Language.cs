85ab9ac5ca37ee7119f317d7e7263ec21cd43d90
Alberto <alberto.izquierdomo@gmail.com>
2024-06-05 11:50:00 +0200

    Create Language model

---
+++ Concert.Models/Language.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Concert.Models
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required field.")]
+        [MaxLength(30)]
+        [DisplayName("Language")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "It must be between 1 and 100.")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
