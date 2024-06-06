ea1c8247893c3efee49a1f42f89bf157198f8338
Alberto <alberto.izquierdomo@gmail.com>
2024-06-07 01:46:50 +0200

    Edit Home view by displaying the list of songs

--- Concert/Areas/Customer/Controllers/HomeController.cs
+++ Concert/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Concert.DataAccess.Repository.IRepository;
 using Concert.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,19 @@ namespace ConcertWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Song> songList = _unitOfWork.Song.GetAll(includeProperties: "Genre,Language");
+
+            return View(songList);
         }
 
         public IActionResult Privacy()
