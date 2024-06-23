e0eccf5ef98493e181681bd7cf98cb4655533fbe
Alberto <alberto.izquierdomo@gmail.com>
2024-06-23 15:49:11 +0200

    Update database with Company ApplicationUser relation

--- Concert.Models/ApplicationUser.cs
+++ Concert.Models/ApplicationUser.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,5 +23,12 @@ namespace Concert.Models
         public string? State { get; set; }
         public string? Country { get; set; }
         public string? PostalCode { get; set; }
+
+        [DisplayName("Company")]
+        public int? CompanyId { get; set; }
+
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company? Company { get; set; }
     }
 }
