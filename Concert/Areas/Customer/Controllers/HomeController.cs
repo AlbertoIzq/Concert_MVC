51dde01232e8aff697be9c875f9e93f730754295
Alberto <alberto.izquierdomo@gmail.com>
2024-06-24 14:15:15 +0200

    Add functionality to AddToSetList button in Details page of SetListSong and add warning notification if song already added to setList

--- Concert/Areas/Customer/Controllers/HomeController.cs
+++ Concert/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Concert.DataAccess.Repository.IRepository;
 using Concert.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace ConcertWeb.Areas.Customer.Controllers
 {
@@ -24,11 +26,60 @@ namespace ConcertWeb.Areas.Customer.Controllers
             return View(songList);
         }
 
-        public IActionResult Details(int id)
+        public IActionResult Details(int songId)
         {
-            Song song = _unitOfWork.Song.Get(u => u.Id == id, includeProperties: "Genre,Language");
+            SetListSong setListSong = new()
+            {
+                Song = _unitOfWork.Song.Get(u => u.Id == songId, includeProperties: "Genre,Language"),
+                SongId = songId,
+                Order = 1 // By default
+            };
 
-            return View(song);
+            return View(setListSong);
+        }
+
+        /// <summary>
+        /// [Authorize] because if someone is posting, they must be an authorized user (no matter which role)
+        /// i.e. they have to be logged into the website
+        /// </summary>
+        /// <param name="setListSong"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(SetListSong setListSong)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            setListSong.ApplicationUserId = userId;
+
+            var setlistSongsFromDb = _unitOfWork.SetListSong.GetAll(u => u.ApplicationUserId == userId);
+
+            if (setlistSongsFromDb != null)
+            {
+                // If it already exists in the db
+                if (setlistSongsFromDb.Where(u => u.SongId == setListSong.SongId).FirstOrDefault() != null)
+                {
+                    TempData["warning"] = "Song already added in setlist";
+                }
+                // If it doesn't, save it as the last song added to setlist
+                else
+                {
+                    var orderedList = setlistSongsFromDb.OrderBy(u => u.Order).ToList();
+                    if (orderedList.Count > 0)
+                    {
+                        setListSong.Order = orderedList.Last().Order + 1;
+                    }
+                    else
+                    {
+                        setListSong.Order = 1;
+                    }
+
+                    _unitOfWork.SetListSong.Add(setListSong);
+                    _unitOfWork.Save();
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
