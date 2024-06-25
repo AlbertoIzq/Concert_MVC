26975f05e64247cccda816dfb097b120028e73f1
Alberto <alberto.izquierdomo@gmail.com>
2024-06-26 00:53:34 +0200

    Create SetListService model and create table in db

---
+++ Concert.Models/SetListService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Concert.Models
+{
+    public class SetListService
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ServiceId { get; set; }
+        [ForeignKey("ServiceId")]
+        [ValidateNever]
+        public Service Service { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
