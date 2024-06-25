c9dec10c85b0d7cd24851912a5ba353495eb5c36
Alberto <alberto.izquierdomo@gmail.com>
2024-06-26 00:44:57 +0200

    Add Services view in HomeController and add API call to add service

--- Concert/Areas/Customer/Controllers/HomeController.cs
+++ Concert/Areas/Customer/Controllers/HomeController.cs
@@ -59,7 +59,7 @@ namespace ConcertWeb.Areas.Customer.Controllers
                 // If it already exists in the db
                 if (setlistSongsFromDb.Where(u => u.SongId == setListSong.SongId).FirstOrDefault() != null)
                 {
-                    TempData["warning"] = "Song already added to setlist";
+                    TempData["warning"] = "Song already added to Set list";
                 }
                 // If it doesn't, save it as the last song added to setlist
                 else
@@ -76,11 +76,24 @@ namespace ConcertWeb.Areas.Customer.Controllers
 
                     _unitOfWork.SetListSong.Add(setListSong);
                     _unitOfWork.Save();
-                    TempData["success"] = "Song added successfully to setlist";
+                    TempData["success"] = "Song added successfully to Set list";
                 }
             }
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Services()
+        {
+            List<Service> serviceList = _unitOfWork.Service.GetAll().OrderBy(u => u.Name).ToList();
+            int indexDefaultService = serviceList.FindIndex(u => u.Id == 1);
+
+            Service defaultService = serviceList[indexDefaultService];
+            for (int i = indexDefaultService; i > 0 ; i--)
+                serviceList[i] = serviceList[i - 1];
+            serviceList[0] = defaultService;
+
+            return View(serviceList);
         }
 
         public IActionResult Privacy()
@@ -93,5 +106,31 @@ namespace ConcertWeb.Areas.Customer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region ApiCalls
+
+        [HttpPost]
+        public IActionResult Services(int? id)
+        {
+            return Json(new { success = true, message = "Service added successfully to Set list" });
+
+            /*
+            Song songToBeDeleted = _unitOfWork.Song.Get(u => u.Id == id);
+            if (songToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            // Delete the old image
+            DeleteOldImage(songToBeDeleted);
+
+            _unitOfWork.Song.Remove(songToBeDeleted);
+            _unitOfWork.Save();
+            //TempData["success"] = "Song deleted successfully";
+            return Json(new { success = true, message = "Song deleted successfully" });
+            */
+        }
+
+        #endregion
     }
 }
