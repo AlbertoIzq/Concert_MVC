14c3f7b684cd998ed9e7434ca7e0b61f6040adc1
Alberto <alberto.izquierdomo@gmail.com>
2024-07-03 12:27:31 +0200

    Create Order Controller and OrderVM

---
+++ Concert.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetailSong> OrderDetailSongs { get; set; }
+		public IEnumerable<OrderDetailService> OrderDetailServices { get; set; }
+    }
+}
---
+++ Concert/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Concert.DataAccess.Data;
+using Concert.DataAccess.Repository.IRepository;
+using Concert.Models;
+using Concert.Models.ViewModels;
+using Concert.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ConcertWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.ROLE_ADMIN)]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region ApiCalls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orderHeaderList });
+        }
+
+        #endregion
+    }
+}
